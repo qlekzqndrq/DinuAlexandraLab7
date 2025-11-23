2bcd36a3c21298e8ef3d0da716d88ba4a3af58d8
Dinu Alexandra <aledinu18@gmail.com>
2025-11-23 04:20:35 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DinuAlexandraLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DinuAlexandraLab7
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
