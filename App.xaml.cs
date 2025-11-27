e2e0455e083d58dc37825695e4f009db56fb4d97
Dinu Alexandra <aledinu18@gmail.com>
2025-11-27 16:13:38 +0200

    Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,30 @@
-﻿namespace DinuAlexandraLab7
+﻿using System;
+using DinuAlexandraLab7.Data;
+using System.IO;
+
+namespace DinuAlexandraLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
+
     }
 }
