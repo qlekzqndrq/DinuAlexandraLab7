a7ff98250fcf79631ad768425dab4dfd3b11167d
Dinu Alexandra <aledinu18@gmail.com>
2025-11-27 17:47:20 +0200

    Laborator 9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,12 +1,12 @@
-using DinuAlexandraLab7.Models;
+﻿using DinuAlexandraLab7.Models;
 namespace DinuAlexandraLab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -20,5 +20,41 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+
+        var selectedProduct = listView.SelectedItem as Product;
+        if (selectedProduct == null)
+        {
+            await DisplayAlert("Eroare", "Te rog selectează un produs din listă pentru a-l șterge.", "OK");
+            return;
+        }
+
+        var shopList = (ShopList)BindingContext;
+
+        await App.Database.DeleteListProductAsync(shopList.ID, selectedProduct.ID);
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+
+        listView.SelectedItem = null;
     }
 }
---
+++ ValidationBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinuAlexandraLab7
+{
+    class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            Editor editor = (Editor)sender;
+
+            if (string.IsNullOrEmpty(args.NewTextValue))
+            {
+                editor.BackgroundColor = Color.FromRgba("#AA4A44");
+                editor.TextColor = Colors.White;
+            }
+            else
+            {
+                editor.BackgroundColor = Colors.White;
+                editor.TextColor = Colors.Black;
+            }
+        }
+    }
+}
